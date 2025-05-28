2c60b17ef63b25f609143ed1e5b961b35012ff43
Sam.Bertillo <bertillosam9@gmail.com>
2025-05-28 22:25:00 +0800

    Added functionality

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+function playerChoice(playerChoice){
+
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+    
+    if(playerChoice === computerChoice){
+        result = "No One Wins!";
+    }
+    else{
+        switch(playerChoice){
+            case "rock":
+                result = (computerChoice === "scissors") ? "You Win" : "You Lose!";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "You Win" : "You Lose!";
+                break;    
+            case "scissors":
+                result = (computerChoice === "paper") ? "You Win" : "You Lose!";
+                break;        
+        }
+    }
+
+    playerDisplay.textContent = `Player: ${playerChoice}`;
+    computerDisplay.textContent = `Bot AI: ${computerChoice}`;
+    resultDisplay.textContent = result;
+}
